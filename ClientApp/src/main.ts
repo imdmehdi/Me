afda6ff5d0eef5d7ad43ea099bf45b5e759bb642
Mehdi Imam <Mehdi.Imam@multichoice.co.za>
2020-12-05 13:32:05 +0530

    error after upgrading to 9

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -20,4 +20,4 @@ if (environment.production) {
 platformBrowserDynamic(providers).bootstrapModule(AppModule)
   .catch(err => console.log(err));
 
-export { renderModule, renderModuleFactory } from '@angular/platform-server';
+//export { renderModule, renderModuleFactory } from '@angular/platform-server';
